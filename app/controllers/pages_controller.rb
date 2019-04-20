262311df945094fd93812c6d89eb3fe9289f79d9
Carlos Lantigua <clantigua954@gmail.com>
2019-04-19 23:06:41 -0400

    adding content to mvc to get used to the structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def about
-
+        # this sets variables that will be used in the view
+        @title = 'About Us'
+        @content = "This is the about page"
     end
 end
