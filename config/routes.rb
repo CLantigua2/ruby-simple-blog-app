7bf3b593e8f08933a5eccc1f4083c87e56cae61a
Carlos Lantigua <clantigua954@gmail.com>
2019-04-20 12:37:29 -0400

    completed base styling and linked nav to pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # the index of the post will be the root, this is in the controller
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   # route to the about page
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   # change post to resource
   # this gives you access to a lot of premade HTTP methods
