beeaf04b56768d2f6f1f8939b6a34461c52814d4
Carlos Lantigua <clantigua954@gmail.com>
2019-04-20 11:17:08 -0400

    added create post, show post and db migration for a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,31 @@ class PostsController < ApplicationController
 
     end
 
+    def show
+        # this will find the post id to display
+        @post = Post.find(params[:id])
+    end
+
     # this will take care of the form
     # "/posts/new"
     def new
 
+    end
+
+    def create
+        # this returns an object with the title and the body that was submitted through the form post
+        # render plain: params[:post].inspect
+        # this step creates a new post with the post params
+        @post = Post.new(post_params)
+        # this step saves the post
+        @post.save
+        # this step will load the show view that will display the post
+        redirect_to @post
+    end
+
+    # this method will define what a post will look like
+    private def post_params
+        # here we grab the post object and shove the title and body attribute into it
+        params.require(:post).permit(:title, :body)
     end
 end
