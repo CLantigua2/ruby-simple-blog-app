af87cf0f109c15b583005d41b760d12286b58c10
Carlos Lantigua <clantigua954@gmail.com>
2019-04-19 22:54:06 -0400

    initializing ruby on rails project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # the index of the post will be the root, this is in the controller
+  root 'posts#index'
+
+  # route to the about page
+  get 'about' => 'pages#about'
+end
